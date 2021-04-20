ae6990beccf085aac5a3c5949c80d79f461ebc53
fentybit <fentymhall@gmail.com>
2021-04-20 16:27:30 -0700

    lab submit

--- src/components/Headquarters.js
+++ src/components/Headquarters.js
@@ -2,30 +2,64 @@ import React, { Component } from 'react';
 import '../stylesheets/Headquarters.css';
 import { Grid } from 'semantic-ui-react';
 import Details from './Details'
-
+import ColdStorage from './ColdStorage';
+import LogPanel from './LogPanel';
 
 class Headquarters extends Component {
-  // Remember, there's many ways to do this. This doesn't have to be a class component. It's up to you.
+  state = {
+    logEvents: [],
+    activated: false
+  }
 
-  render(){
-    return(
+  addLog = (event) => {
+    this.setState({
+      logEvents: [event, ...this.state.logEvents]
+    })
+  }
+
+  handleActivate = () => {
+    this.setState({
+      activated: !this.state.activated
+    })
+  }
+
+  selectedHost = () => this.props.hosts.find(host => host.id === this.props.selectedHostId)
+
+  renderDecomHosts = () => this.props.hosts.filter(host => !host.active)
+
+  render() {
+    const { areas, hosts, activateHost, selectedHostId, selectHost, setArea, activeAll } = this.props
+
+    return (
       <Grid celled='internally'>
         <Grid.Column width={8}>
-
-        {/* Something goes here.... */}
-
+          <ColdStorage
+            hosts={this.renderDecomHosts()}
+            selectHost={selectHost}
+            selectedHostId={selectedHostId}
+          />
         </Grid.Column>
         <Grid.Column width={5}>
-          <Details />
+          <Details
+            hosts={hosts}
+            activateHost={activateHost}
+            selectedHost={this.selectedHost()}
+            areas={areas}
+            setArea={setArea}
+            addLog={this.addLog}
+          />
         </Grid.Column>
         <Grid.Column width={3}>
-
-        {/* and here. Take visual cues from the screenshot/video in the Readme. */}
-
+          <LogPanel
+            handleActivate={this.handleActivate}
+            events={this.state.logEvents}
+            activated={this.state.activated}
+            addLog={this.addLog}
+          />
         </Grid.Column>
       </Grid>
     )
   }
 }
 
-export default Headquarters;
+export default Headquarters
--- src/components/HostList.js
+++ src/components/HostList.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import { Card } from 'semantic-ui-react'
+import Host from './Host'
 
-const HostList = () => {
+const HostList = ({ hosts, selectHost, selectedHostId, limit }) => {
+  const renderHosts = () => (
+    hosts.map(({ id, imageUrl }) => {
+      if (selectedHostId === id) {
+        return <Host
+          imageUrl={imageUrl}
+          id={id} key={id}
+          selectHost={selectHost}
+          selected
+        />
+      } else {
+        return <Host
+          imageUrl={imageUrl}
+          id={id} key={id}
+          selectHost={selectHost}
+        />
+      }
+    })
+  )
 
-  return(
+  return (
     <Card.Group itemsPerRow={6}>
-      {/* What do you think, partner? */}
+      {renderHosts()}
     </Card.Group>
   )
 }
